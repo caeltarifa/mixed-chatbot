83cedf8b2385319ddafac84af268940e7907ba4a
caeltarifa <caeltarifa@gmail.com>
2025-04-19 23:40:06 -0300

    Refreshing ideas

--- BACKUP/workflows/action_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start the Rasa Action server to handle custom actions
-echo "Starting Rasa Action server..."
-rasa run actions --debug
--- workflows/rasa_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start the Rasa server to handle NLU and dialogue
-echo "Starting Rasa server..."
-rasa run --enable-api --cors "*" --debug
